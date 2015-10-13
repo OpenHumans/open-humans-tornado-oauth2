230bc28060d8aeae736ef7fd07312c5b598e413c
Beau Gunderson <beau@beaugunderson.com>
2015-10-12 21:56:40 -0700

    linting

--- open_humans_tornado_oauth2.py
+++ open_humans_tornado_oauth2.py
@@ -12,17 +12,16 @@ class OpenHumansMixin(auth.OAuth2Mixin):
     OpenHumans OAuth Mixin, based on FacebookGraphMixin
     """
 
-    _OAUTH_AUTHORIZE_URL = 'https://openhumans.org/oauth2/authorize/'
-    _OAUTH_ACCESS_TOKEN_URL = 'https://openhumans.org/oauth2/token/'
+    _OAUTH_AUTHORIZE_URL = 'https://www.openhumans.org/oauth2/authorize/'
+    _OAUTH_ACCESS_TOKEN_URL = 'https://www.openhumans.org/oauth2/token/'
 
-    _API_URL = 'https://openhumans.org/api'
+    _API_URL = 'https://www.openhumans.org/api'
 
     @auth._auth_return_future
     def get_authenticated_user(self, redirect_uri, client_id, client_secret,
                                code, callback):
         """
-        Handles the login for OpenHumans, queries /user and returns a user
-        object
+        Handle the login for OpenHumans, query /user and return a user object.
         """
         http = httpclient.AsyncHTTPClient()
 
@@ -36,7 +35,7 @@ class OpenHumansMixin(auth.OAuth2Mixin):
 
         http.fetch(self._OAUTH_ACCESS_TOKEN_URL,
                    functools.partial(self._on_access_token_cb, callback),
-                   method="POST",
+                   method='POST',
                    headers={
                        'Content-Type': 'application/x-www-form-urlencoded'
                    },
@@ -45,7 +44,7 @@ class OpenHumansMixin(auth.OAuth2Mixin):
     @staticmethod
     def _on_access_token_cb(future, response):
         """
-        callback for authentication url, if successful get the user details
+        Callback for authentication URL, if successful get the user details.
         """
         if response.error:
             logger.warning('OpenHumans auth error: %s', str(response))
@@ -70,7 +69,7 @@ class OpenHumansMixin(auth.OAuth2Mixin):
     def open_humans_request(self, path, callback, access_token=None,
                             method='GET', body=None, **args):
         """
-        Makes an Open Humans API request, rerturns the parsed data
+        Make an Open Humans API request, rerturn the parsed data.
         """
         args['access_token'] = access_token
         args['format'] = 'json'
@@ -94,7 +93,7 @@ class OpenHumansMixin(auth.OAuth2Mixin):
     @staticmethod
     def _parse_response(callback, response):
         """
-        Parse the JSON from the API
+        Parse the JSON from the API.
         """
         if response.error:
             logger.warning('HTTP error from OpenHumans: %s', response.error)
